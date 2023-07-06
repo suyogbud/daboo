38002a670ab49aab4c1e5a81136089f254ee4b2e
Suyog Singh Budhathoki <suyogbc10@gmail.com>
2023-07-06 03:08:33 -0400

    added functionality to calculate score

--- script.js
+++ script.js
@@ -17,7 +17,23 @@ function rollDice() {
 
 // Function to calculate the score
 function calculateScore(rolls) {
-    //TODO
+  let score = 0;
+
+  // Calculate the score based on the dice rolls
+  // You need to implement the scoring rules of the Ferengi Game of Daboo here.
+
+  // Example scoring logic (modify according to the game rules):
+  for (const roll of rolls) {
+    if (roll === 1) {
+      score += 10;
+    } else if (roll === 6) {
+      score += 6;
+    } else {
+      score += roll;
+    }
+  }
+
+  return score;
 }
 
 // Event listener for the "Roll Dice" button
